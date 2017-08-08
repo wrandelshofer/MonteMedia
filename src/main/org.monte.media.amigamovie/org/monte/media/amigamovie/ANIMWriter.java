c33029857b6f634f998c082a0ec7a959d0c5813c
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2017-08-08 08:11:14 +0200

    Reformats code.

--- src/main/org.monte.media.amigamovie/org/monte/media/amigamovie/ANIMWriter.java
+++ src/main/org.monte.media.amigamovie/org/monte/media/amigamovie/ANIMWriter.java
@@ -4,16 +4,22 @@
  */
 package org.monte.media.amigamovie;
 
-import org.monte.media.codec.Format;
-import org.monte.media.movie.MovieWriter;
-import org.monte.media.math.Rational;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import org.monte.media.anim.ANIMMultiplexer;
-import static org.monte.media.codec.FormatKeys.*;
-import static org.monte.media.codec.audio.AudioFormatKeys.*;
-import static org.monte.media.anim.AmigaVideoFormatKeys.*;
+import static org.monte.media.anim.AmigaVideoFormatKeys.ENCODING_ANIM_OP5;
+import static org.monte.media.anim.AmigaVideoFormatKeys.toCAMG;
+import org.monte.media.codec.Format;
+import static org.monte.media.codec.FormatKeys.EncodingKey;
+import static org.monte.media.codec.FormatKeys.MIME_ANIM;
+import org.monte.media.codec.FormatKeys.MediaType;
+import static org.monte.media.codec.FormatKeys.MediaTypeKey;
+import static org.monte.media.codec.FormatKeys.MimeTypeKey;
+import static org.monte.media.codec.video.VideoFormatKeys.DataClassKey;
+import static org.monte.media.codec.video.VideoFormatKeys.FixedFrameRateKey;
+import org.monte.media.math.Rational;
+import org.monte.media.movie.MovieWriter;
 
 /**
  * {@code ANIMWriter}.
@@ -22,7 +28,9 @@ import static org.monte.media.anim.AmigaVideoFormatKeys.*;
  * @version $Id: ANIMWriter.java 364 2016-11-09 19:54:25Z werner $
  */
 public class ANIMWriter extends ANIMMultiplexer implements MovieWriter {
-    public final static Format ANIM=new Format(MediaTypeKey,MediaType.FILE,MimeTypeKey,MIME_ANIM);
+
+    public final static Format ANIM = new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_ANIM);
+
     @Override
     public Format getFileFormat() throws IOException {
         return ANIM;
@@ -34,35 +42,37 @@ public class ANIMWriter extends ANIMMultiplexer implements MovieWriter {
     }
 
     private static class Track {
+
         Format format;
     }
-    private ArrayList<Track> tracks=new ArrayList<Track>();
-    
+    private ArrayList<Track> tracks = new ArrayList<Track>();
+
     public ANIMWriter(File file) throws IOException {
         super(file);
     }
 
     @Override
     public int addTrack(Format format) throws IOException {
-        if (tracks.size()>0) throw new UnsupportedOperationException("only 1 track supported");
-        Format derivedFormat=format.prepend(
-                MediaTypeKey,MediaType.VIDEO,MimeTypeKey,MIME_ANIM,
-                EncodingKey,ENCODING_ANIM_OP5,DataClassKey,byte[].class,//
-               FixedFrameRateKey,false);
-        
+        if (tracks.size() > 0) {
+            throw new UnsupportedOperationException("only 1 track supported");
+        }
+        Format derivedFormat = format.prepend(
+                MediaTypeKey, MediaType.VIDEO, MimeTypeKey, MIME_ANIM,
+                EncodingKey, ENCODING_ANIM_OP5, DataClassKey, byte[].class,//
+                FixedFrameRateKey, false);
+
         setCAMG(toCAMG(derivedFormat));
-        Track tr=new Track();
-        tr.format=derivedFormat;
-        
+        Track tr = new Track();
+        tr.format = derivedFormat;
+
         tracks.add(tr);
-        return tracks.size()-1;
+        return tracks.size() - 1;
     }
 
     @Override
     public Format getFormat(int track) {
         return tracks.get(track).format;
     }
-    
 
     @Override
     public int getTrackCount() {
@@ -82,6 +92,5 @@ public class ANIMWriter extends ANIMMultiplexer implements MovieWriter {
     public boolean isEmpty(int track) {
         return inputTime.isZero();
     }
-    
-    
+
 }
