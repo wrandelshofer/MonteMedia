c317bc1112d28ff8aa62c09f9649d5b3aa25911a
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2018-12-24 11:36:13 +0100

    Revert "Reorganizes sources."
    This reverts commit b7615ff6

--- modules/org.monte.media/src/org/monte/media/mp3/package-info.java
+++
@@ -1,9 +0,0 @@
-/* @(#)package-info.java
- * Copyright © 2017 Werner Randelshofer, Switzerland. MIT License.
- */
-
-/**
- * Provides classes for reading MP3 elementary streams.
- */
-
-package org.monte.media.mp3;
