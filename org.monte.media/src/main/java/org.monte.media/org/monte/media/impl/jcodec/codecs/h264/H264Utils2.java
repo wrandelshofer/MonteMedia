99c0b5b59be179abffa463934b7a19c0b7e005a0
wrandelshofer <werner.randelshofer@fibermail.ch>
2025-02-10 13:09:26 +0100

    Fix compilation error.

--- org.monte.media/src/main/java/org.monte.media/org/monte/media/impl/jcodec/codecs/h264/H264Utils2.java
+++ org.monte.media/src/main/java/org.monte.media/org/monte/media/impl/jcodec/codecs/h264/H264Utils2.java
@@ -1,4 +1,6 @@
-package org.monte.media.impl.jcodec.codecs.h264;/**
+package org.monte.media.impl.jcodec.codecs.h264;
+
+/**
  * NotImplementedException.
  * <p>
  * References:
@@ -9,8 +11,8 @@ package org.monte.media.impl.jcodec.codecs.h264;/**
  *     <br><a href="https://github.com/jcodec/jcodec/blob/7e5283408a75c3cdbefba98a57d546e170f0b7d0/LICENSE">BSD 2-Clause License.</a></dt>
  *     <dd><a href="https://github.com/jcodec/jcodec">github.com</a></dd>
  * </dl>
- */ {
-
+ */
+public class H264Utils2 {
     public static int golomb2Signed(int val) {
         int sign = ((val & 0x1) << 1) - 1;
         val = ((val >> 1) + (val & 0x1)) * sign;
